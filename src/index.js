e75e7c3894802a9694e80a932aa8347ac5921306
syed shaheer <sdshaheer5421@gmail.com>
2024-06-25 20:52:50 +0530

    added cors for socket

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const server = app.listen(config.port, () => {
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: 'http://localhost:5173'
+        origin: '*'
     }
 })
 
